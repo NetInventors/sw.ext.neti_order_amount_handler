f363b6649cd7ad182711fe6d76771b80c00bb077
Jan-Lasse Rombach <janlasserombach@gmail.com>
2019-02-18 15:34:24 +0100

    change let to var

--- Resources/frontend/js/hidePaymentSelect.js
+++ Resources/frontend/js/hidePaymentSelect.js
@@ -9,7 +9,7 @@ $.extend(Neti, {
     'OrderAmountHandler': {
         'hidePaymentSelect': function () {
             if (typeof netiHidePaymentSelect !== 'undefined' && netiHidePaymentSelect) {
-                let $paymentMethodList = $('.payment--method-list');
+                var $paymentMethodList = $('.payment--method-list');
 
                 $paymentMethodList.find('.alert.is--info.is--hidden').removeClass('is--hidden');
                 $paymentMethodList.find('.panel--body').addClass('is--hidden');
